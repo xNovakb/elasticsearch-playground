f3dd23f94b697ea1f345fe170e31b3ad7290f50e
xnovakb <noboris.gymnazium@gmail.com>
2025-11-06 15:31:00 +0100

    refactor: rename test class to AbstractTest

--- src/test/java/com/xnovakb/elasticPlayground/AbstractTest.java
+++ src/test/java/com/xnovakb/elasticPlayground/AbstractTest.java
@@ -6,10 +6,8 @@ import org.springframework.context.annotation.Import;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
-class ElasticsearchPlaygroundApplicationTests {
+public class AbstractTest {
 
-	@Test
-	void contextLoads() {
-	}
+
 
 }
