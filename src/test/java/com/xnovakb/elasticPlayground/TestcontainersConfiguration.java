8102cc819c3e2d082e200c609b19bb7da1a09214
xnovakb <noboris.gymnazium@gmail.com>
2025-11-06 15:25:59 +0100

    Squashed commit of the following:
    commit 77c1bff18db338808bccd7e5de642b13065d4e17
    Author: xnovakb <noboris.gymnazium@gmail.com>
    Date:   Thu Nov 6 15:24:55 2025 +0100
    
        refactor: change version of elasticsearch docker image and add env variables in TestcontainersConfiguration

--- src/test/java/com/xnovakb/elasticPlayground/TestcontainersConfiguration.java
+++ src/test/java/com/xnovakb/elasticPlayground/TestcontainersConfiguration.java
@@ -9,10 +9,15 @@ import org.testcontainers.utility.DockerImageName;
 @TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
 
+	public static final String XPACK_SECURITY_ENABLED = "xpack.security.enabled";
+	public static final String XPACK_SECURITY_HTTP_SSL_ENABLED = "xpack.security.http.ssl.enabled";
+
 	@Bean
 	@ServiceConnection
 	ElasticsearchContainer elasticsearchContainer() {
-		return new ElasticsearchContainer(DockerImageName.parse("docker.elastic.co/elasticsearch/elasticsearch:7.17.10"));
+		return new ElasticsearchContainer(DockerImageName.parse("docker.elastic.co/elasticsearch/elasticsearch:9.0.2"))
+			.withEnv(XPACK_SECURITY_ENABLED, Boolean.FALSE.toString())
+			.withEnv(XPACK_SECURITY_HTTP_SSL_ENABLED, Boolean.FALSE.toString());
 	}
 
 }
