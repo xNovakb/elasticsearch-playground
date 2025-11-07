5b2f7cff845e8ba9438175e9373325a80f16eef9
xnovakb <noboris.gymnazium@gmail.com>
2025-11-07 18:46:01 +0100

    feature: create bulk crud operation test with el repository

--- src/test/java/com/xnovakb/elasticPlayground/CrudOperationTest.java
+++ src/test/java/com/xnovakb/elasticPlayground/CrudOperationTest.java
@@ -6,6 +6,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.util.Streamable;
+
+import java.util.List;
+import java.util.stream.IntStream;
 
 @Slf4j
 public class CrudOperationTest extends AbstractTest {
@@ -19,20 +23,50 @@ public class CrudOperationTest extends AbstractTest {
         repo.save(employee);
 
         printAll();
-        var employeeSaved = repo.findById(1).orElseThrow();
+        var employeeSavedMutable = repo.findById(1).orElseThrow();
 
-        Assertions.assertEquals(employee.toString(), employeeSaved.toString());
+        Assertions.assertEquals(employee.toString(), employeeSavedMutable.toString());
 
-        employeeSaved.setAge(27);
-        employeeSaved = repo.save(employeeSaved);
+        employeeSavedMutable.setAge(27);
+        employeeSavedMutable = repo.save(employeeSavedMutable);
 
         printAll();
-        Assertions.assertEquals(employeeSaved.getAge(), 27);
+        Assertions.assertEquals(employeeSavedMutable.getAge(), 27);
 
         repo.deleteById(1);
 
         printAll();
         Assertions.assertTrue(repo.findById(1).isEmpty());
+    }
+
+    @Test
+    public void bulkCrud() {
+        final var employees = IntStream.rangeClosed(1, 10)
+            .mapToObj(i -> createEmployee(i, "name-" + 1, 25 + i))
+            .toList();
+
+        repo.saveAll(employees);
+
+        printAll();
+        Assertions.assertEquals(10, repo.count());
+
+        final var ids = List.of(2, 4, 6);
+        final var iterableEmployeeHits = repo.findAllById(ids);
+        final var employeeHits = Streamable.of(iterableEmployeeHits).toList();
+
+        Assertions.assertEquals(3, employeeHits.size());
+
+        employeeHits.forEach(e -> e.setAge(e.getAge() + 10));
+        repo.saveAll(employeeHits);
+
+        repo.findAllById(ids).forEach(
+            e -> Assertions.assertEquals(e.getId() + 35, e.getAge())
+        );
+
+        repo.deleteAllById(ids);
+
+        printAll();
+        Assertions.assertEquals(7, repo.count());
     }
 
     private Employee createEmployee(Integer id, String name, Integer age) {
